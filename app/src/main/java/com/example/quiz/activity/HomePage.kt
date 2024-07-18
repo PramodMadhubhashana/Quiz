e634f3de99d3c7d74751fe88f5164ab340805f4a
Pramod Madhubhashana <108177082+PramodMadhubhashana@users.noreply.github.com>
2024-07-18 15:02:57 +0530

    add

--- app/src/main/java/com/example/quiz/activity/HomePage.kt
+++ app/src/main/java/com/example/quiz/activity/HomePage.kt
@@ -1,10 +1,11 @@
-package com.example.quiz
+package com.example.quiz.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quiz.R
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/quiz/activity/RankScreen.kt
+++ app/src/main/java/com/example/quiz/activity/RankScreen.kt
@@ -1,4 +1,4 @@
-package com.example.quiz
+package com.example.quiz.activity
 
 import android.os.Bundle
 import android.widget.ImageView
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quiz.R
 
 class RankScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
