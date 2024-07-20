8eb8c80046594f4713e91e04a754f6e76d9ed388
Pramod Madhubhashana <108177082+PramodMadhubhashana@users.noreply.github.com>
2024-07-20 11:12:20 +0530

    adpter Backend

---
+++ app/src/main/java/com/example/quiz/adapter/QuestionAdapter.kt
@@ -0,0 +1,159 @@
+package com.example.quiz.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.quiz.R
+import com.example.quiz.databinding.ActivityMainBinding
+import com.example.quiz.databinding.ViewholderQuestionBinding
+import com.example.quiz.domain.QuestionModel
+import com.example.quiz.domain.useModel
+
+
+class QuestionAdapter (
+    val CorrectAnswer:String?,
+    val users : MutableList<String> = mutableListOf(),
+    val returnScore: score
+    ):RecyclerView.Adapter<QuestionAdapter.Viewholder>() {
+        interface score{
+            fun amount(number:Int,clickedAnswer: String)
+        }
+
+
+
+
+    private  lateinit var binding: ViewholderQuestionBinding
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionAdapter.Viewholder {
+        val inflater = LayoutInflater.from(parent.context)
+        binding= ViewholderQuestionBinding.inflate(inflater,parent, false)
+        return Viewholder()
+    }
+
+    override fun onBindViewHolder(holder: QuestionAdapter.Viewholder, position: Int) {
+        val binding=ViewholderQuestionBinding.bind(holder.itemView)
+        binding.answerTxt.text=differ.currentList[position]
+
+        var currentPos=0
+        when(CorrectAnswer){
+            "a"->{
+                currentPos=0
+            }
+            "b"->{
+                currentPos=1
+            }
+            "c"->{
+                currentPos=2
+            }
+            "d"->{
+                currentPos=3
+            }
+
+        }
+        if(differ.currentList.size==5 && currentPos==position){
+            binding.answerTxt.setBackgroundResource(R.drawable.green)
+            binding.answerTxt.setTextColor(
+                ContextCompat.getColor(binding.root.context,
+                    R.color.white
+                )
+            )
+            val drawable = ContextCompat.getDrawable(binding.root.context, R.drawable.tick)
+            binding.answerTxt.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null, drawable, null)
+        }
+        if(differ.currentList.size==5){
+            var clickedPos=0
+            when(differ.currentList[4]){
+                "a"->{
+                    clickedPos=0
+                }
+                "b"->{
+                    clickedPos=1
+                }
+                "c"->{
+                    clickedPos=2
+                }
+                "d"->{
+                    clickedPos=3
+                }
+            }
+            if(clickedPos==position && clickedPos!=currentPos){
+                binding.answerTxt.setBackgroundResource(R.drawable.red)
+                binding.answerTxt.setTextColor(
+                    ContextCompat.getColor(binding.root.context,
+                        R.color.white
+                    )
+                )
+                val drawable = ContextCompat.getDrawable(binding.root.context, R.drawable.thieves)
+                binding.answerTxt.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null, drawable, null)
+            }
+        }
+        if(position==4){
+            binding.root.visibility=View.GONE
+        }
+        holder.itemView.setOnClickListener{
+            var str=""
+            when(position){
+                0->{
+                    str="a"
+                }
+                1->{
+                    str="b"
+                }
+                2->{
+                    str="c"
+                }
+                3->{
+                    str="d"
+                }
+            }
+            users.add(4,str)
+            notifyDataSetChanged()
+
+            if(currentPos==position){
+                binding.answerTxt.setBackgroundResource(R.drawable.green)
+                binding.answerTxt.setTextColor(
+                    ContextCompat.getColor(binding.root.context,
+                        R.color.white
+                    )
+                )
+                val drawable = ContextCompat.getDrawable(binding.root.context, R.drawable.tick)
+                binding.answerTxt.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null, drawable, null)
+                returnScore.amount(5,str)
+            }
+            else{
+                binding.answerTxt.setBackgroundResource(R.drawable.red)
+                binding.answerTxt.setTextColor(
+                    ContextCompat.getColor(binding.root.context,
+                        R.color.white
+                    )
+                )
+                val drawable = ContextCompat.getDrawable(binding.root.context, R.drawable.thieves)
+                binding.answerTxt.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null, drawable, null)
+                returnScore.amount(0,str)
+            }
+        }
+
+        if(differ.currentList.size==5) holder.itemView.setOnClickListener(null)
+
+
+
+    }
+
+    override fun getItemCount()=differ.currentList.size
+
+    inner class Viewholder:RecyclerView.ViewHolder(binding.root)
+
+    private val differCallBack= object : DiffUtil.ItemCallback<String>(){
+        override fun areItemsTheSame(oldItem: String, newItem: String):Boolean{
+            return oldItem==newItem
+        }
+        override fun areContentsTheSame(oldItem: String, newItem: String):Boolean{
+            return oldItem==newItem
+        }
+    }
+    val differ= AsyncListDiffer(this,differCallBack)
+}
